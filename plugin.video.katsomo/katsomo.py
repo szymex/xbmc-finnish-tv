128abe5cc7fff5d326cba087637b30143a018237
szymex <szymex@gmail.com>
2012-11-15 19:28:33 +0200

    improved scapper, added groups

--- plugin.video.katsomo/katsomo.py
+++ plugin.video.katsomo/katsomo.py
@@ -1,99 +1,62 @@
 # -*- coding: utf-8 -*-
-import urllib,urllib2,re,xbmcplugin,xbmcgui
-import os,subprocess, json
-import CommonFunctions
+import xbmcplugin,xbmcgui
 import xbmcutil as xbmcUtil
-import inspect
-import time
-import datetime
-from datetime import date
 import sys
+from katsomoscrapper import KatsomoScrapper
+from datetime import datetime, date
 
-common = CommonFunctions
-common.plugin = "plugin.video.katsomo"
 
 #sets default encoding to utf-8
 reload(sys) 
 sys.setdefaultencoding('utf8')
-
-USER_AGENT = 'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Safari/535.19'
-
-def scrapVideoLink(url):
-	
-	xbmc.log( url )
-	req = urllib2.Request(url)
-	req.add_header('User-Agent', USER_AGENT)
-	response = urllib2.urlopen(req)
-	ret = common.parseDOM(response.read(), "source", {'type': 'video/mp4'}, ret = "src")
-	if len(ret)>0:
-		return ret[0]
-	else:
-		return None
-
-def scrapSerie(url):
-	xbmc.log( url )
-	req = urllib2.Request(url)
-	req.add_header('User-Agent', USER_AGENT)
-	response = urllib2.urlopen(req)
-	ret = common.parseDOM(response.read(), "div", {'class': 'program'})
-	
-	l = []
-	for r in ret:
-		link = 'http://m.katsomo.fi' + common.parseDOM(r, "a", ret = "href")[0]
-		title = common.parseDOM(r, "p", {'class': 'program-name'})[0]
-		title += ' ' + common.parseDOM(r, "p", {'class': 'program-abstract'})[0]
-		img = 'http://m.katsomo.fi' + common.parseDOM(r, "img", ret = "src")[0]
-		l.append( {'link':link, 'title':title, 'img':img} )
-		
-	return l
-
-def scrapPrograms():
-	req = urllib2.Request('http://m.katsomo.fi/katsomo/programs')
-	req.add_header('User-Agent', USER_AGENT)
-	response = urllib2.urlopen(req)
-	ret = common.parseDOM(response.read(), "ul", {'class': 'all-programs-list'})
-	retIDs = common.parseDOM(ret, "li", ret="data-id")
-	retNames = common.parseDOM(ret, "li")
-	xbmc.log( str(retIDs) )
-	l=[]
-	for i in range(0, len(retIDs)):
-		name = retNames[i]
-		id = retIDs[i]
-		if not 'star' in name:
-			l.append({'title':name, 'link':'http://m.katsomo.fi/katsomo/?treeId=' + id})
-	
-	return l
-	
 	
 class KatsomoAddon (xbmcUtil.ViewAddonAbstract):
 	ADDON_ID = 'plugin.video.katsomo'
+	GROUP = u'   [COLOR blue]%s[/COLOR]'
 	
 	def __init__(self):
 		xbmcUtil.ViewAddonAbstract.__init__(self)
 		self.addHandler(None, self.handleMain)
 		self.addHandler('serie', self.handleSerie)
 		self.addHandler('programs', self.handlePrograms)
+		self.scrapper = KatsomoScrapper()
 
 	def handleMain(self, pg, args):
 		self.addViewLink('›› Ohjelmat','programs',1 )
-		self.addViewLink('Uutiset','serie',1, {'link':'http://m.katsomo.fi/katsomo/?treeId=33001'} )
-		self.addViewLink('Urheilu','serie',1, {'link':'http://m.katsomo.fi/katsomo/?treeId=33002'} )
-		self.addViewLink('Lapset','serie',1, {'link':'http://m.katsomo.fi/katsomo/?treeId=33003'} )
+		self.addViewLink('Uutiset','serie',1, {'link':'http://m.katsomo.fi/katsomo/?treeId=33001', 'useGroups': True} )
+		self.addViewLink('Urheilu','serie',1, {'link':'http://m.katsomo.fi/katsomo/?treeId=33002', 'useGroups': True} )
+		self.addViewLink('Lapset','serie',1, {'link':'http://m.katsomo.fi/katsomo/?treeId=33003', 'useGroups': True} )
 		
 	def handlePrograms(self, pg, args):
-		programs = scrapPrograms()
+		programs = self.scrapper.scrapPrograms()
 		for p in programs:
 			self.addViewLink(p['title'] ,'serie',1,{'link': p['link']})
 
 	def handleSerie(self, pg, args):
 		link = args['link']
-		series = scrapSerie(link)
+		series = self.scrapper.scrapSerie(link)
+		xbmcplugin.setContent(int(sys.argv[1]), 'episodes')
+		useGroups = args['useGroups'] if 'useGroups' in args else False;
+		groupName = formatDate(datetime.now())
+		
 		for s in series:
-			self.addVideoLink(s['title'] , s['link'], s['img'])
+			if useGroups and s['publ-ts'] != None and groupName != formatDate(s['publ-ts']):
+				groupName = formatDate(s['publ-ts'])
+				self.addVideoLink(self.GROUP % groupName, '', '')
+
+			self.addVideoLink(s['title'] , s['link'], s['img'], infoLabels={'aired': s['published'] } )
 		
 	def handleVideo(self, link):
-		return scrapVideoLink(link)
+		return self.scrapper.scrapVideoLink(link)
 #-----------------------------------
+
+def formatDate(dt):
+	delta = date.today() - dt.date()
+	if delta.days==0: return 'Today'
+	if delta.days==1: return 'Yesterday'
+	if delta.days>1 and delta.days<5: return dt.strftime('%A %d.%m.%Y')
+	return dt.strftime('%d.%m.%Y')
+
 
 katsomo = KatsomoAddon()
 katsomo.handle()
---
+++ plugin.video.katsomo/katsomoscrapper.py
@@ -0,0 +1,68 @@
+import urllib,urllib2,re
+import CommonFunctions
+import xbmc
+from datetime import datetime
+import time
+
+common = CommonFunctions
+common.plugin = "plugin.video.katsomo"
+
+USER_AGENT = 'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Safari/535.19'
+
+class KatsomoScrapper:
+
+	def scrapVideoLink(self, url):
+		xbmc.log( url )
+		req = urllib2.Request(url)
+		req.add_header('User-Agent', USER_AGENT)
+		response = urllib2.urlopen(req)
+		ret = common.parseDOM(response.read(), "source", {'type': 'video/mp4'}, ret = "src")
+		if len(ret)>0:
+			return ret[0]
+		else:
+			return None
+			
+	def scrapSerie(self, url):
+		xbmc.log( url )
+		req = urllib2.Request(url)
+		req.add_header('User-Agent', USER_AGENT)
+		response = urllib2.urlopen(req)
+		ret = common.parseDOM(response.read(), "div", {'class': 'program'})
+		
+		l = []
+		for r in ret:
+			link = 'http://m.katsomo.fi' + common.parseDOM(r, "a", ret = "href")[0]
+			title = common.parseDOM(r, "p", {'class': 'program-name'})[0]
+			title += ' ' + common.parseDOM(r, "p", {'class': 'program-abstract'})[0]
+			img = 'http://m.katsomo.fi' + common.parseDOM(r, "img", ret = "src")[0]
+			
+			timestamp = common.parseDOM(r, "p", {'class': 'timestamp'})[0]
+			ts = None
+			if not 'TULOSSA' in timestamp:
+				try:
+					ts = datetime.strptime(timestamp.replace('- ', ''), '%d.%m.%Y %H.%M')
+				except TypeError:
+					ts = datetime(*(time.strptime(timestamp.replace('- ', ''), '%d.%m.%Y %H.%M')[0:6]))				
+			else:
+				title = 'TULOSSA: ' + title
+			
+			l.append( {'link':link, 'title':title, 'img':img, 'published': timestamp, 'publ-ts': ts} )
+			
+		return l
+
+	def scrapPrograms(self):
+		req = urllib2.Request('http://m.katsomo.fi/katsomo/programs')
+		req.add_header('User-Agent', USER_AGENT)
+		response = urllib2.urlopen(req)
+		ret = common.parseDOM(response.read(), "ul", {'class': 'all-programs-list'})
+		retIDs = common.parseDOM(ret, "li", ret="data-id")
+		retNames = common.parseDOM(ret, "li")
+		#xbmc.log( str(retIDs) )
+		l=[]
+		for i in range(0, len(retIDs)):
+			name = retNames[i]
+			id = retIDs[i]
+			if not 'star' in name:
+				l.append({'title':name, 'link':'http://m.katsomo.fi/katsomo/?treeId=' + id, 'treeId': id})
+		
+		return l
