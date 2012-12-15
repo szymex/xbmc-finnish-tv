1e99346d3f9787c735094d3f0782bc50e09a371e
szymex <szymex@gmail.com>
2012-12-15 12:54:04 +0200

    fixed user-agent in katsomo plugin

--- plugin.video.katsomo/katsomoscrapper.py
+++ plugin.video.katsomo/katsomoscrapper.py
@@ -7,7 +7,7 @@ import time
 common = CommonFunctions
 common.plugin = "plugin.video.katsomo"
 
-USER_AGENT = 'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Safari/535.19'
+USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3'
 
 class KatsomoScrapper:
 
